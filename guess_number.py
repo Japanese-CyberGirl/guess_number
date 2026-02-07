11bed21bbf5066edeccb87edbda1dcd1b38e06e6
Max Levashov <chinese_anime_girl@mail.ru>
2026-02-07 13:36:01 +0700

    guess_number changes

--- guess_number.py
+++ guess_number.py
@@ -1,19 +1,20 @@
 from random import randint
 
-x = randint(0,100)
+x = randint(0, 100)
 
 answer = int(input())
 
-while (answer != x):
+while answer != x:
     if answer < x:
-        print('Ваше число меньше того, что загадано')
+        print("Ваше число меньше того, что загадано")
         answer = int(input())
         continue
     if answer > x:
-        print('Ваше число больше того, что загадано')
+        print("Ваше число больше того, что загадано")
         answer = int(input())
         continue
-print('Отличная интуиция! Вы угадали число :)')
+print("Отличная интуиция! Вы угадали число :)")
+
 
 def fumo():
     pass
